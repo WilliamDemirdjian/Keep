d85425d69fa912c00c43c8b82d69bb5534629ad8
WilliamDemirci <wil.8dev@gmail.com>
2020-03-13 11:20:58 +0100

    optimize imports

--- app/src/main/java/com/wil8dev/keep/NoteAdapter.kt
+++ app/src/main/java/com/wil8dev/keep/NoteAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import androidx.core.view.marginBottom
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_note.view.*
 
